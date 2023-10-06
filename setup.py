b6cc41ecad930e9b723a009399a20545a78c140a
Trung Bui <trung@t7.sg>
2023-10-06 22:59:18 +0700

    upd deps. list

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-import os
-from setuptools import setup, find_packages
+from os import environ
+
+from setuptools import find_packages, setup
+
+GIT_SSH_KEY = environ.get('GIT_SSH_KEY', '')
 
 setup(
     name="zipline_bundles",
     version="0.1",
     packages=[".", "zipline-bundles"],
-    entry_points = {
+    entry_points={
         'console_scripts': ['zipline-bundles-install=install:main'],
     },
     install_requires=[
@@ -13,5 +16,6 @@ setup(
         'logbook',
         'python-binance',
         'yahoofinancials',
-   ]
+        f'py-datasource @ git+https://${GIT_SSH_KEY}@github.com/TsevenSG/py-datasource.git',
+    ]
 )
