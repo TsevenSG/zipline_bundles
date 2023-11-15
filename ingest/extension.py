b2f87b4d6a690b774f0a2478a902976d0ef98b4f
Trung Bui <trung@t7.sg>
2023-11-15 12:06:46 +0700

    register treasury bundle

--- ingest/extension.py
+++ ingest/extension.py
@@ -66,6 +66,18 @@ register('tv_latin_america', # bundle's name
 )
 
 from zipline.data.bundles import tradingview
+register('tv_treasury', # bundle's name
+         direct_ingester('TRADINGVIEW',
+                         every_min_bar=False,
+                         symbol_list_env='SYMS', # the environment variable holding the comma separated list of assert names
+                         downloader=tradingview.get_downloader(start_date='1990-01-01',
+                                                               end_date='2024-01-01'
+                         ),
+         ),
+         calendar_name='NYSE',
+)
+
+from zipline.data.bundles import tradingview
 register('tv_custom', # bundle's name
          direct_ingester('TRADINGVIEW',
                          every_min_bar=False,
